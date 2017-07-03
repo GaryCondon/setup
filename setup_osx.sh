18ebd2873b82de0423ef55ab48bb9a92d5584846
Gary Condon <gary@condon.net>
2017-07-03 01:13:39 -0400

    updated setup_osx.sh

--- setup_osx.sh
+++ setup_osx.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-# configure dock
+# reset dock to default
+# defaults delete com.apple.dock
 
 # remove all apps from dock
 defaults delete com.apple.dock persistent-apps
+
+# only show running apps in dock
+# defaults write com.apple.dock static-only -bool TRUE
 
 # remove all folders from dock
 defaults delete com.apple.dock persistent-others
@@ -17,12 +21,22 @@ defaults write com.apple.dock autohide -bool true
 # remove autohide delay
 defaults write com.apple.dock autohide-delay -float 0
 
-# only show running apps in dock
-# defaults write com.apple.dock static-only -bool TRUE
+# minimize windows into application icons
+defaults write com.apple.dock minimize-to-application -bool true
 
-# reset dock to default
-# defaults delete com.apple.dock
+# don’t show dashboard as a space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# don’t rearrange spaces by most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# set bottom left hot corner to activate screen saver
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # restart dock
 killall Dock &> /dev/null
-
