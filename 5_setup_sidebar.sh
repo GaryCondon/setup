cbcf037de951ccb20bd83e96f4873e977c793963
Gary Condon <gary@condon.net>
2017-07-04 18:02:53 -0400

    add mysides to remove sidebar items

---
+++ 5_setup_sidebar.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# install mysides to remove items from sidebar
+# git clone https://github.com/mosen/mysides.git
+# ./mysides/make dmg # must mount dmg & run pkg to install to /usr/local/bin
+
